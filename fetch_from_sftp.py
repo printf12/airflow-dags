801827fc20ea87371a20568a9fce8424878a20c2
Oussema Acheche <oussema.acheche@nagler-company.com>
2024-06-03 12:01:16 +0200

    Add example DAG_SFTP

--- fetch_from_sftp.py
+++ fetch_from_sftp.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.providers.sftp.sensors.sftp import SFTPSensor
 from airflow.providers.sftp.operators.sftp import SFTPOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.utils.dates import days_ago
 
 default_args = {
@@ -12,7 +13,7 @@ with DAG('fetch_from_sftp', default_args=default_args, schedule_interval='@daily
 
     check_file_update = SFTPSensor(
         task_id='check_file_update',
-        path='/upload/test_file.txt',
+        path='/home/foo/upload/test_file.txt',
         poke_interval=60,
         timeout=600,
         sftp_conn_id='sftp_default',
@@ -22,8 +23,14 @@ with DAG('fetch_from_sftp', default_args=default_args, schedule_interval='@daily
         task_id='download_file',
         ssh_conn_id='sftp_default',
         local_filepath='/tmp/downloaded_test_file.txt',
-        remote_filepath='/upload/test_file.txt',
+        remote_filepath='/home/foo/upload/test_file.txt',
         operation='get',
     )
 
-    check_file_update >> download_file
+    execute_less = SSHOperator(
+        task_id='execute_less',
+        ssh_conn_id='ssh_default',
+        command='powershell.exe -File C:\\path\\to\\less_script.ps1'
+    )
+
+    check_file_update >> download_file >> execute_less
